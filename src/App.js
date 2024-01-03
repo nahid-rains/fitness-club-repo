924185514f8637bd6296b0013efe26e48033de81
nahid-rains <nahidrupom143@gmail.com>
2024-01-03 21:59:10 +0600

    correction

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Club from './components/Club/Club';
 import Header from './components/Header/Header';
-import Test from './components/Test/Test';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Club/>
-      <Test/>
+  
     </div>
   );
 }
